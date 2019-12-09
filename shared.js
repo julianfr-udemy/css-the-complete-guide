de34648963cd1529ae198352c07af1bac9df0c08
Julián <jfernandezreyes@anac.gob.ar>
2019-12-09 15:24:27 -0300

    9-Working with JavaScript & CSS

---
+++ shared.js
@@ -0,0 +1,25 @@
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var mobileNav = document.querySelector('.mobile-nav');
+
+function showModal(show = true) {
+  if (show) {
+    [backdrop, modal].forEach(element => { if (element) { element.classList.add("open"); } });
+  } else {
+    [backdrop, modal, mobileNav].forEach(element => { if (element) { element.classList.remove("open"); } });
+  }
+}
+
+document
+  .querySelectorAll(".plan button")
+  .forEach(button => button.addEventListener("click", () => showModal()));
+
+[document.querySelector(".modal__action--negative"), backdrop]
+  .forEach(element => { if (element) { element.addEventListener("click", () => showModal(false)) } });
+
+document
+  .querySelector('.toggle-button')
+  .addEventListener("click", () => {
+    mobileNav.classList.add("open");
+    backdrop.classList.add("open");
+  });

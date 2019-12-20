306087d8c31cb0e27513f679799ce4ac98aeee00
Julián <jfernandezreyes@anac.gob.ar>
2019-12-20 10:10:08 -0300

    16-Transitions & Animations in CSS

--- shared.js
+++ shared.js
@@ -1,6 +1,7 @@
 const backdrop = document.querySelector(".backdrop");
 const modal = document.querySelector(".modal");
 const mobileNav = document.querySelector('.mobile-nav');
+const mainNavCta = document.querySelector('.main-nav__item--cta');
 
 function showModal(show = true) {
   if (show) {
@@ -23,3 +24,7 @@ document
     mobileNav.classList.add("open");
     backdrop.classList.add("open");
   });
+
+mainNavCta.addEventListener('animationstart', event => console.log('Animation started', event));
+mainNavCta.addEventListener('animationend', event => console.log('Animation ended', event));
+mainNavCta.addEventListener('animationiteration', event => console.log('Animation iteration', event));
